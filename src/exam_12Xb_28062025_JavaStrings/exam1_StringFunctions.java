a7153a12c6e4ce89c188b02b715bc8d267535fbd
Uday Kumar R <uday3323@gmail.com>
2025-06-28 10:41:18 +0530

    26 th June 2025 Strings Coding Exam Code push

---
+++ src/exam_12Xb_28062025_JavaStrings/exam1_StringFunctions.java
@@ -0,0 +1,10 @@
+package exam_12Xb_28062025_JavaStrings;
+
+public class exam1_StringFunctions {
+    public static void main(String[] args){
+        String s1 = "Hello World";
+        System.out.printf("Length : %d ", s1.length() );
+        System.out.printf("First char : %s ",s1.charAt(0));
+        System.out.printf("substring : %s ", s1.substring(6,11));
+    }
+}
